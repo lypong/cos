11698c259950f68ed41afc22821626f2ff5962c9
seb <sfv.jacob@gmail.com>
2024-05-03 19:47:15 +0200

    Actually interesting

--- cbasic.js
+++ cbasic.js
@@ -1,8 +1,7 @@
-const keywords = ["PRINT"]
+const keywords = ["LET"];
 const TokenType = {
-  number : "number",
-  identifier : "identifier",
-  keyword : "keyword",
+  integer : "integer",
+  word : "word",
   less : "less",
   lessOrEqual : "lessOrEqual",
   greater : "greater",
@@ -33,11 +32,15 @@ class Token {
     this.lexeme = lexeme;
     this.literal = literal;
   }
+  asString() {
+    return `Token { type : ${this.type}, lexeme : ${this.lexeme}, literal : ${this.literal}}`;
+  }
 }
 class Lexer {
   constructor(code) {
     this.code = code;
     this.position = 0;
+    this.lexedKeyword = false;
   }
   atEnd(offset) {
     if(offset === undefined)
@@ -62,18 +65,20 @@ class Lexer {
     this.consume();
     return true;
   }
-  keywordOrIdentifier(c){
+  word(c){
     let start = this.position-1;
     let peek = this.peek();
     while(isLetter(peek)||isDigit(peek)||peek==='_'){
       this.consume();
-      if(keywords.includes(this.code.substring(start,this.position)))
-        return new Token(TokenType.keyword,this.code.substring(start,this.position));
+      if(!this.lexedKeyword&&keywords.includes(this.code.substring(start,this.position))){
+        this.lexedKeyword = true;
+        return new Token(TokenType.word,this.code.substring(start,this.position));
+      }
       peek = this.peek();
     }
-    return new Token(TokenType.identifier,this.code.substring(start,this.position));
+    return new Token(TokenType.word,this.code.substring(start,this.position));
   }
-  number(c){
+  integer(c){
     let start = this.position-1;
     let peek = this.peek();
     while(isDigit(peek)){
@@ -81,14 +86,104 @@ class Lexer {
       peek = this.peek();
     }
     let lexeme = this.code.substring(start,this.position);
-    return new Token(TokenType.number,lexeme,parseInt(lexeme));
+    return new Token(TokenType.integer,lexeme,parseInt(lexeme));
+  }
+}
+class Parser {
+  constructor(tokens) {
+    this.tokens = tokens;
+    this.position = 0;
+  }
+  atEnd(offset) {
+    if(offset === undefined)
+      offset = 0;
+    return this.position + offset >=this.tokens.length;
+  }
+  consume() {
+    if (this.atEnd())
+      return null;
+    return this.tokens[this.position++];
+  }
+  peek(offset) {
+    if(offset === undefined)
+      offset = 0;
+    if(this.atEnd(offset))
+      return null;
+    return this.tokens[this.position+offset];
+  }
+  matchAndConsume(t) {
+    if(this.peek().type!==t)
+      return false;
+    this.consume();
+    return true;
+  }
+  num() {
+    if(this.peek().type===TokenType.minus && this.peek(1).type===TokenType.integer)
+      return new Node(this.consume().type,0,this.consume().literal);
+    if(this.peek().type===TokenType.integer) 
+      return new Node(TokenType.plus,0,this.consume().literal);
+    return null;
+  }
+
+  expr() {
+    let lhs = this.num();
+    //TODO Support more complex expr.
+
+    return lhs;
+  }
+}
+class Node{
+  constructor(operator,lhs,rhs){
+    this.operator = operator;
+    this.lhs = lhs;
+    this.rhs = rhs;
+  }
+  evaluate(){
+    let evaluatedLhs;
+    let evaluatedRhs;
+    if(this.lhs.evaluate===undefined)
+      evaluatedLhs = this.lhs;
+    else
+      evaluatedLhs = this.lhs.evaluate();
+
+    if(this.rhs.evaluate===undefined)
+      evaluatedRhs = this.rhs;
+    else
+      evaluatedRhs = this.rhs.evaluate();
+    switch(this.operator) {
+      case TokenType.plus:
+        return evaluatedLhs+evaluatedRhs;
+      case TokenType.minus:
+        return evaluatedLhs-evaluatedRhs;
+      case TokenType.mult:
+        return evaluatedLhs*evaluatedRhs;
+      case TokenType.div:
+        return evaluatedLhs/evaluatedRhs;
+      case TokenType.exp:
+        return evaluatedLhs**evaluatedRhs;
+    }
   }
 }
 function isDigit(c) {
+  if(c===null)
+    return false;
   return c >= '0' && c <= '9';
 }
 function isLetter(c) {
+  if(c===null)
+    return false;
   return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
+function isOperator(type) {
+  switch(type){
+      case TokenType.plus:
+      case TokenType.minus:
+      case TokenType.mult:
+      case TokenType.div:
+      case TokenType.exp:
+        return true;
+  }
+  return false;
 }
 
 function lex(code){
@@ -116,15 +211,15 @@ function lex(code){
       case '\r':
         break;
       case '0':
-        tokens.push(new Token(TokenType.number,'0',0));
+        tokens.push(new Token(TokenType.integer,'0',0));
         break;
       default:
         if(singleCharacterTokens[c]!==undefined)
           tokens.push(new Token(singleCharacterTokens[c],c));
         else if(isLetter(c))
-          tokens.push(lexer.keywordOrIdentifier());
+          tokens.push(lexer.word());
         else if(isDigit(c)&&c!='0')
-          tokens.push(lexer.number());
+          tokens.push(lexer.integer());
         else
           console.log(`Char ${c} can't be tokenized.`)
         break;
@@ -133,4 +228,42 @@ function lex(code){
   return tokens;
 }
 
+function parse(tokens){
+  let parser = new Parser(tokens);
+  let lineNumber = parser.consume();
+  if(lineNumber.type!=TokenType.integer) {
+    console.log(`Expected line number got ${lineNumber.asString()}`);
+    return;
+  }
+  let keyword = parser.consume(); 
+  switch(keyword.lexeme) {
+    case "LET":
+      let variableName = parser.consume();
+      if(variableName.type!==TokenType.word){
+        console.log(`Expected identifier got ${variableName.asString()}`);
+        return;
+      }
+      if(keywords.includes(variableName.lexeme)) {
+        console.log(`Expected var name got keyword instead : ${variableName.asString()}`);
+        return;
+      }
+      if(!parser.matchAndConsume(TokenType.equal)){
+        console.log(`Expected = after var name.`);
+        return;
+      }
+      let expr = parser.expr();
+      //TODO implement actual behaviour.
+      console.log(`I WILL make var : ${variableName.lexeme} equal to ${expr.evaluate()}`)
+      break;
+    default:
+      console.log(`Expected keyword got ${keyword}`);
+      return;
+  }
+  if(!parser.atEnd()) //TODO DELETE
+    console.log(`Tokens weren't entirely consumed.`); //TODO DELETE
+}
+
 console.log(lex("- + = <= <> <+ PRINTyak y12_a >= 1023 0"));
+console.log(lex("10LETLET=4"))
+console.log(parse(lex("10LETpip=4")));
+//console.log(parse(lex("10LETLET=4")));
