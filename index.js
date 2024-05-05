0d2fd8499dceb11972c12e301131be239bb36296
seb <sfv.jacob@gmail.com>
2024-05-05 21:57:49 +0200

    Create basic website

---
+++ index.js
@@ -0,0 +1,19 @@
+let inputElement=document.getElementById("in");
+let input = "";
+let p = new Program();
+
+addEventListener("keydown",(event)=>{
+    if(event.key==="Backspace")
+        input=input.substring(0,input.length-1);
+    if(event.key==="Enter"){
+        bPrint(input);
+        if(input==="RUN")
+            p.runProgram();
+        else
+            p.writeLine(input);
+        input = "";
+    }
+    else if(event.key.length===1)
+        input+=event.key;
+    inputElement.textContent = input;
+})
--- src/utils.ts
+++ src/utils.ts
@@ -9,5 +9,14 @@ function isLetter(c : string) : boolean {
   return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
 }
 function bPrint(...args : any[]){
-  console.log(...args);
+  let termElement=document.getElementById("term");
+  args.forEach((arg)=>`${arg}`.split('\n').forEach((l)=>{
+      let p = document.createElement("p");
+      p.textContent = l;
+      if(termElement===null) {
+        console.error("div with id 'term' does not exist");
+        return;
+      } else
+        termElement.appendChild(p);
+  }))
 }
